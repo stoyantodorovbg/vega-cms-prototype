114088136a695040e0f471e07af5ef0d8efcb69d
stoyantodorovbg <stoyan_11@abv.bg>
2019-11-03 22:28:35 +0200

    phrase halper caches results

--- app/Helpers/Helpers.php
+++ app/Helpers/Helpers.php
@@ -1,10 +1,22 @@
 <?php
 
 use App\Models\Phrase;
+use Illuminate\Support\Facades\Cache;
 
 if (!function_exists('phrase')) {
     function phrase($systemName)
     {
-        return Phrase::where('system_name', $systemName)->first()->text;
+        if($value = Cache::get('phrase_' . app()->getLocale() . '_' . $systemName)) {
+            return $value;
+        }
+
+        if($phrase = Phrase::where('system_name', $systemName)->first()) {
+            $phrase = Phrase::where('system_name', $systemName)->first();
+            Cache::put('phrase_' . app()->getLocale() . '_' . $systemName, $phrase->text, 10000000000);
+
+            return $phrase->text;
+        }
+
+        return $systemName;
     }
 }
--- tests/Feature/PhraseTest.php
+++ tests/Feature/PhraseTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use App\Models\Phrase;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -30,5 +31,32 @@ class PhraseTest extends TestCase
         $phraseText = phrase('example_phrase');
 
         $this->assertEquals($phraseText, 'фраза на български');
+    }
+
+    /** @test */
+    public function phrase_helper_caches_the_phrase()
+    {
+        factory(Phrase::class)->create([
+            'system_name' => 'example_phrase',
+            'text' => [
+                'en' => 'a phrase in English',
+                'bg' => 'фраза на български',
+            ]
+        ]);
+
+        app()->setLocale('bg');
+        phrase('example_phrase');
+
+        $this->assertTrue(Cache::has('phrase_' . app()->getLocale() . '_' . 'example_phrase'));
+    }
+
+    /** @test */
+    public function phrase_helper_returns_the_accepted_phrase_key_if_there_is_no_such_phrase_in_the_cache_or_db()
+    {
+        Cache::forget('example_phrase');
+
+        $result = phrase('example_phrase');
+
+        $this->assertEquals($result, 'example_phrase');
     }
 }
